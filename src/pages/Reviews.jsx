4caf70c085bccc22a10be4650d3eb33e8bdb7f19
daud <alpn.ds@mail.ru>
2024-01-12 18:29:58 +0300

    commit

--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -5,75 +5,77 @@ import ReviewsRequest from "../API/reviewsRequest";
 import axios from "axios";
 
 const Reviews = () => {
-    const params = useParams();
-    const productId = params.productId;
+        const params = useParams();
+        const productId = params.productId;
 
-    const [reviews, setReviews] = useState([]);
+        const [reviews, setReviews] = useState([]);
 
-    const [inputName, setInputName] = useState('');
-    const [inputReview, setInputReview] = useState('');
+        const [inputName, setInputName] = useState('');
+        const [inputReview, setInputReview] = useState('');
 
-    function handleInputChange(setInput) {
-        return function (event) {
-            setInput(event.target.value);
+        function handleInputChange(setInput) {
+            return function (event) {
+                setInput(event.target.value);
+            }
         }
-    }
 
-    function handleSendPostRequest(event) {
-        return async function () {
+        function handleSendPostRequest(event) {
+            return async function () {
+                event.preventDefault();
+                if (inputName && inputReview !== '') {
+                    const data = {
+                        product_id: productId,
+                        author: inputName,
+                        rating: 3,
+                        content: inputReview,
+                    };
+                    const response = await axios.post(process.env.API_URL + "api/reviews", data);
+                    await reviewsFetch();
 
-            event.preventDefault();
-            const data = {
-                product_id: productId,
-                author: inputName,
-                rating: 3,
-                content: inputReview,
-            };
-            const response = await axios.post(process.env.API_URL + "api/reviews", data);
-            await reviewsFetch();
+                    setInputReview('');
+                    setInputName('');
 
-            setInputReview('');
-            setInputName('');
+                }
+            }
+        }
+
+        async function reviewsFetch() {
+            const data = await ReviewsRequest.productReviews(productId);
+            setReviews(data);
 
         }
-    }
 
-    async function reviewsFetch() {
-        const data = await ReviewsRequest.productReviews(productId);
-        setReviews(data);
-
-    }
-
-    useEffect(() => {
-        reviewsFetch();
-    }, []);
-    return (
-        <div className="reviews">
-            <div className="reviews__wrapper container">
-                <div className="reviews__wrapper-inner">
-                    <h2>Отзывы</h2>
-                    <form className="reviews__form">
-                        <div className="reviews__form-input-wrapper">
+        useEffect(() => {
+            reviewsFetch();
+        }, []);
+        return (
+            <div className="reviews">
+                <div className="reviews__wrapper container">
+                    <div className="reviews__wrapper-inner">
+                        <h2>Отзывы</h2>
+                        <form className="reviews__form">
+                            <div className="reviews__form-input-wrapper">
                             <span className="reviews__form-input-title">
                                 Ваше имя
                             </span>
-                            <input value={inputName} onChange={handleInputChange(setInputName)} type="text"
-                                   className="main-style-input"/>
-                        </div>
-                        <div className="reviews__form-input-wrapper">
+                                <input maxLength='25' required value={inputName} onChange={handleInputChange(setInputName)} type="text"
+                                       className="main-style-input"/>
+                            </div>
+                            <div className="reviews__form-input-wrapper">
                             <span className="reviews__form-input-title">
                                 Ваш отзыв
                             </span>
-                            <input value={inputReview} onChange={handleInputChange(setInputReview)} type="text"
-                                   className="main-style-input"/>
-                        </div>
-                        <input onClick={handleSendPostRequest(event)} type="submit" className="main-style-button"/>
-                    </form>
-                    <ReviewsList reviews={reviews}/>
+                                <textarea maxLength='900' required value={inputReview} onChange={handleInputChange(setInputReview)}
+                                          className="main-style-input"/>
+                            </div>
+                            <input  onClick={handleSendPostRequest(event)} type="submit" className="main-style-button"/>
+                        </form>
+                        <ReviewsList reviews={reviews}/>
+                    </div>
                 </div>
             </div>
-        </div>
-    );
-};
+        );
+    }
+;
 
 export default Reviews;
