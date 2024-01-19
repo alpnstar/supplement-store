73d6229b842c7974dc086b7b7551a2307c771d84
daud <alpn.ds@mail.ru>
2024-01-19 14:05:15 +0300

    commit

--- src/components/Products/ProductCard/ProductsCard.jsx
+++ src/components/Products/ProductCard/ProductsCard.jsx
@@ -5,25 +5,76 @@ import {useNavigate, useParams} from "react-router";
 import ProductsRequest from "../../../API/productsRequest";
 import ProductsItemInner from "../ProductsItemInner";
 import Error from "../../../pages/Error";
+import axios from "axios";
+import ReviewsRequest from "../../../API/reviewsRequest";
 
 const ProductCard = ({setCartItems}) => {
 
     const navigate = useNavigate();
     const params = useParams();
     const [product, setProduct] = useState();
-    const [error, setError] = useState();
+    const [reviewsExpanded, setReviewsExpanded] = useState(false);
+
+    const productId = params.productId;
+
+    const [reviews, setReviews] = useState([]);
+    const [ReviewsError, setReviewsError] = useState();
+
+    const [inputName, setInputName] = useState('');
+    const [inputReview, setInputReview] = useState('');
+
+    function handleInputChange(setInput) {
+        return function (event) {
+            setInput(event.target.value);
+        }
+    }
+
+    function handleSendPostRequest(event) {
+        return async function (event) {
+            try {
+                const data = {
+                    product_id: productId,
+                    author: inputName,
+                    rating: 5,
+                    content: inputReview,
+                };
+                await axios.post(process.env.API_URL + "api/reviews", data);
+                await reviewsFetch();
+                setReviewsError();
+
+
+            } catch (error) {
+                setReviewsError(error);
+                console.log(error);
+
+            } finally {
+                setInputReview('');
+                setInputName('');
+            }
+        }
+    }
+
+    async function reviewsFetch() {
+        const data = await ReviewsRequest.productReviews(productId);
+        setReviews(data);
+
+    }
+
+    useEffect(() => {
+        reviewsFetch();
+        window.scrollTo({
+            top: 0,
+            behavior: 'auto' // Добавление плавности прокрутки (не обязательно)
+        });
+    }, []);
 
     async function productFetch() {
         try {
             const response = await ProductsRequest.allProducts.getById(params.productId)
             setProduct(response.data);
         } catch (error) {
-            setError(error);
+            setReviewsError(error);
         }
-    }
-
-    function handleNavigate() {
-        navigate('../reviews/' + product.id);
     }
 
     useEffect(() => {
@@ -49,11 +100,36 @@ const ProductCard = ({setCartItems}) => {
                         <div className="productCard__reviews">
                             <div className="productCard__reviews-title">
                                 <h2>Отзывы</h2>
-                                <button onClick={handleNavigate} className="second-style-button">
+                                <button onClick={()=> setReviewsExpanded(!reviewsExpanded)} className="second-style-button">
                                     Оставить отзыв
                                 </button>
                             </div>
-                            <ReviewsList productView={false} full={true} reviews={product.attributes.reviews}/>
+                                <form className={`reviews__form ${reviewsExpanded ? 'reviews__form--expanded' : ''}`}>
+                                    <div className="reviews__form-input-wrapper">
+                                <span
+                                    className="form-input-error">{ReviewsError && ReviewsError.response.data.errors['author'] && ReviewsError.response.data.errors['author']}</span>
+                                        <span className="reviews__form-input-title">
+                                Ваше имя
+                            </span>
+                                        <input maxLength='25' required value={inputName}
+                                               onChange={handleInputChange(setInputName)}
+                                               type="text"
+                                               className="main-style-input"/>
+                                    </div>
+                                    <div className="reviews__form-input-wrapper">
+                                <span
+                                    className="form-input-error">{ReviewsError && ReviewsError.response.data.errors['content'] && ReviewsError.response.data.errors['content']}</span>
+                                        <span className="reviews__form-input-title">
+                                Ваш отзыв
+                            </span>
+                                        <textarea maxLength='900' required value={inputReview}
+                                                  onChange={handleInputChange(setInputReview)}
+                                                  className="main-style-input"/>
+                                    </div>
+                                    <input onClick={handleSendPostRequest()} type="button" value='Отправить'
+                                           className="main-style-button"/>
+                                </form>
+                            <ReviewsList productView={false} full={true} reviews={reviews}/>
                         </div>
                     </div>
                     <div className="productCard__content-right">
@@ -61,7 +137,7 @@ const ProductCard = ({setCartItems}) => {
                     </div>
                 </div>
             </div>
-            : error ? <Error>Товар не найден</Error> : ''
+            : ReviewsError ? <Error>Товар не найден</Error> : ''
     )
 };
 
--- src/pages/Reviews.jsx
+++ src/pages/Reviews.jsx
@@ -6,87 +6,12 @@ import axios from "axios";
 
 const Reviews = () => {
         const params = useParams();
-        const productId = params.productId;
 
-        const [reviews, setReviews] = useState([]);
-        const [error, setError] = useState();
-
-        const [inputName, setInputName] = useState('');
-        const [inputReview, setInputReview] = useState('');
-
-        function handleInputChange(setInput) {
-            return function (event) {
-                setInput(event.target.value);
-            }
-        }
-
-        function handleSendPostRequest(event) {
-            return async function (event) {
-                try {
-                    const data = {
-                        product_id: productId,
-                        author: inputName,
-                        rating: 3,
-                        content: inputReview,
-                    };
-                    const response = await axios.post(process.env.API_URL + "api/reviews", data);
-                    await reviewsFetch();
-                    setError();
-
-
-                } catch (error) {
-                    setError(error);
-                    console.log(error);
-
-                } finally {
-                    setInputReview('');
-                    setInputName('');
-                }
-            }
-        }
-
-        async function reviewsFetch() {
-            const data = await ReviewsRequest.productReviews(productId);
-            setReviews(data);
-
-        }
-
-        useEffect(() => {
-            reviewsFetch();
-            window.scrollTo({
-                top: 0,
-                behavior: 'auto' // Добавление плавности прокрутки (не обязательно)
-            });
-        }, []);
         return (
             <div className="reviews">
                 <div className="reviews__wrapper container">
                     <div className="reviews__wrapper-inner">
                         <h2>Отзывы</h2>
-                        <form className="reviews__form">
-                            <div className="reviews__form-input-wrapper">
-                                <span
-                                    className="form-input-error">{error && error.response.data.errors['author'] && error.response.data.errors['author']}</span>
-                                <span className="reviews__form-input-title">
-                                Ваше имя
-                            </span>
-                                <input maxLength='25' required value={inputName} onChange={handleInputChange(setInputName)}
-                                       type="text"
-                                       className="main-style-input"/>
-                            </div>
-                            <div className="reviews__form-input-wrapper">
-                                <span
-                                    className="form-input-error">{error && error.response.data.errors['content'] && error.response.data.errors['content']}</span>
-                                <span className="reviews__form-input-title">
-                                Ваш отзыв
-                            </span>
-                                <textarea maxLength='900' required value={inputReview}
-                                          onChange={handleInputChange(setInputReview)}
-                                          className="main-style-input"/>
-                            </div>
-                            <input onClick={handleSendPostRequest()} type="button" value='Отправить'
-                                   className="main-style-button"/>
-                        </form>
                         <ReviewsList full={true} reviews={reviews}/>
                     </div>
                 </div>
